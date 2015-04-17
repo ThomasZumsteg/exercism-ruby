7ba04325d1ed64bed1b249d91056deafb510faab
Tom <thomas.zumsteg@gmail.com>
2015-04-17 16:11:10 -0700

    Second iteration hamming with zip

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,11 +1,5 @@
 class Hamming
-	def self.compute dna_a, dna_b
-		hamming = 0
-		(0..([dna_a.length, dna_b.length].min - 1)).each do |i|
-			if dna_a[i] != dna_b[i]
-				hamming += 1
-			end
-		end
-		return hamming
-	end
+  def self.compute dna_a, dna_b
+    dna_a.chars.zip(dna_b.chars).count{ |a,b| a != b }
+  end
 end
