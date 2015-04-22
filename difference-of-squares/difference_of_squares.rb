72e215e2c398b80adeb91b20094184285444c2b3
Tom <thomas.zumsteg@gmail.com>
2015-04-22 07:53:30 -0700

    Second iteration difference_of_squares now with functions

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,11 +1,20 @@
 class Squares
   # Square sequence sums and differences
-  attr_reader :square_of_sums, :sum_of_squares, :difference
 
   def initialize(num)
-    @square_of_sums = num**2 * (num + 1)**2 / 4
-    @sum_of_squares = num * (num + 1) * (2*num + 1) / 6
-    @difference = @square_of_sums - @sum_of_squares
+    @num = num
+  end
+
+  def difference
+    square_of_sums - sum_of_squares
+  end
+
+  def square_of_sums 
+    @square_of_sums ||= @num**2 * (@num + 1)**2 / 4
+  end
+
+  def sum_of_squares
+    @sum_of_squares ||= @num * (@num + 1) * (2*@num + 1) / 6
   end
 
 end
