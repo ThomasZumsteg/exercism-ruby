a85470ccf834fd43f944bd3b561f6c2a6f7495ab
Tom <thomas.zumsteg@gmail.com>
2015-04-23 12:06:21 -0700

    Prettied up roman_numerals

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,16 +1,16 @@
- DIGITS = [ [''] * 4,
-            'I X C M'.split(' '), #1
-            'II XX CC MM'.split(' '), #2
-            'III XXX CCC MMM'.split(' '), #3
-            'IV XL CD MMMM'.split(' '), #4
-            'V L D'.split(' '), #5
-            'VI LX DC'.split(' '), #6
-            'VII LXX DCC'.split(' '), #7
+module RomanNumerals
+ DIGITS = [[   '',  '',  '',   ''], #0
+            '   I    X    C    M'.split(' '), #1
+            '  II   XX   CC   MM'.split(' '), #2
+            ' III  XXX  CCC  MMM'.split(' '), #3
+            '  IV   XL   CD'.split(' '), #4
+            '   V    L    D'.split(' '), #5
+            '  VI   LX   DC'.split(' '), #6
+            ' VII  LXX  DCC'.split(' '), #7
             'VIII LXXX DCCC'.split(' '), #8
-            'IX XC CM'.split(' '), #9
+            '  IX   XC   CM'.split(' '), #9
           ]
 
-class Integer
   def to_roman
     # Roman numeral representation of a number
     return self.to_s.reverse.each_char.with_index.map do |digit, power| 
@@ -19,3 +19,4 @@ class Integer
   end
 end
 
+Fixnum.class_eval { include RomanNumerals }
