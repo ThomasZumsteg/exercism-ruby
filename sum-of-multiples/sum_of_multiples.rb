6901c3154ce2883c0bc5551d3a7fcbb6d7be63d3
Tom <thomas.zumsteg@gmail.com>
2015-05-03 17:13:18 -0700

    Second iteration sum_of_multiples

--- sum-of-multiples/sum_of_multiples.rb
+++ sum-of-multiples/sum_of_multiples.rb
@@ -1,15 +1,13 @@
 class SumOfMultiples
-  @multiples = [3, 5]
-
   def initialize(*multiples)
     @multiples = multiples
   end
 
   def to(num)
-    return (0...num).select { |n| @multiples.any? { |m| n % m == 0}}.inject(:+)
+    (0...num).select { |n| @multiples.any? { |m| n % m == 0}}.inject(:+)
   end
 
   def self.to(num)
-    return (0...num).select { |n| @multiples.any? { |m| n % m == 0}}.inject(:+)
+    new(3,5).to(num)
   end
 end
