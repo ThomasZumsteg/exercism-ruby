1d30afe6bb63d243c585d968385e8ed9765270f0
Tom <thomas.zumsteg@gmail.com>
2015-05-06 17:03:30 -0700

    Atomized food_chain structure, second iteration

--- food-chain/food_chain.rb
+++ food-chain/food_chain.rb
@@ -1,8 +1,7 @@
 class FoodChainSong
   # Sings the food chain song
-  @@lyrics = {
-    opening: "I know an old lady who swallowed ",
-    firstLine: [
+  def initialize
+    @first_line = [
       "",
       "a fly.\n",
       "a spider.\nIt wriggled and jiggled and tickled inside her.\n",
@@ -12,8 +11,9 @@ class FoodChainSong
       "a goat.\nJust opened her throat and swallowed a goat!\n",
       "a cow.\nI don't know how she swallowed a cow!\n",
       "a horse.\nShe's dead, of course!\n",
-    ],
-    refrain: [
+    ]
+    @opening = "I know an old lady who swallowed "
+    @refrain = [
       "I don't know why she swallowed the fly. Perhaps she'll die.\n",
       "She swallowed the spider to catch the fly.\n",
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n",
@@ -21,26 +21,35 @@ class FoodChainSong
       "She swallowed the dog to catch the cat.\n",
       "She swallowed the goat to catch the dog.\n",
       "She swallowed the cow to catch the goat.\n",
-    ],
-  }
-
-  def verse(v)
-    # A single verse
-    words = @@lyrics[:opening] + @@lyrics[:firstLine][v]
-
-    # Horse case: She's dead, of course
-    return words if @@lyrics[:refrain].length <= v - 1
-    
-    words + @@lyrics[:refrain][0, v].reverse.join
+    ]
+    @num_verses = @refrain.length
   end
 
-  def verses(stop, start)
+  def verse(verse_num)
+    # A single verse
+    words = @opening + first_line(verse_num)
+
+    # Horse case: She's dead, of course
+    return words if @num_verses < verse_num
+    
+    words + refrain(verse_num)
+  end
+
+  def verses(start, stop)
     # A range of verses
-    return [*stop..start].inject('') { |song, v| song + verse(v) + "\n" }
+    start.upto(stop).inject('') { |song, v| song + verse(v) + "\n" }
   end
 
   def sing
     # The whole song
-    return verses(1, 8)
+    verses(1, 8)
+  end
+
+  def first_line(verse_num)
+    @first_line[verse_num]
+  end
+
+  def refrain(verse_num)
+    @refrain.first(verse_num).reverse.join
   end
 end
