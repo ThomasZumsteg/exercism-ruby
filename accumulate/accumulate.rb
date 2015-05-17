27885d3232b566f5efb0e78d0013c949c8113f55
Tom <thomas.zumsteg@gmail.com>
2015-05-16 21:10:43 -0700

    Second iteration Accumulate

--- accumulate/accumulate.rb
+++ accumulate/accumulate.rb
@@ -1,9 +1,10 @@
 module Accumulate
-  def accumulate( &func )
+  def accumulate( &block )
     # Doesn't use map or collect or etc.
+    return enum_for(:accumulate) unless block_given?
     modified = []
-    self.each{ |e| modified << func.call(e) }
-    return modified
+    each{ |e| modified << block.call(e) }
+    modified
   end
 end
 
