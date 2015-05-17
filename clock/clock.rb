25426cd0e0e1824136f842c2352859c81003553c
Tom <thomas.zumsteg@gmail.com>
2015-05-16 21:26:14 -0700

    Second iteration clock

--- clock/clock.rb
+++ clock/clock.rb
@@ -8,7 +8,7 @@ class Clock
 
   def to_s
     # Looks like 01:07
-    "%02d:%02d" % [@hours, @minutes]
+    sprintf("%02d:%02d", @hours, @minutes)
   end
 
   def +(other)
@@ -23,10 +23,8 @@ class Clock
 
   def ==(other)
     # Test of two clock are equal
-    self.to_s == other.to_s
+    to_s == other.to_s
   end
-
-  private
 
   def initialize(hours, minutes)
     dh, @minutes = minutes.divmod(60)
