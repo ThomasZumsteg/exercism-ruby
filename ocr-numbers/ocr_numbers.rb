f407c07d1f3bbb19144873603ad2da5ed72e2f66
Tom <thomas.zumsteg@gmail.com>
2015-06-01 16:27:20 -0700

    first iteration ocr_numbers

---
+++ ocr-numbers/ocr_numbers.rb
@@ -0,0 +1,58 @@
+class OCR
+  @@digits = [
+ " _ \n"+
+ "| |\n"+
+ "|_|\n",
+
+ "   \n"+
+ "  |\n"+
+ "  |\n",
+
+ " _ \n"+
+ " _|\n"+
+ "|_ \n",
+
+ " _ \n"+
+ " _|\n"+
+ " _|\n",
+
+ "   \n"+
+ "|_|\n"+
+ "  |\n",
+
+ " _ \n"+
+ "|_ \n"+
+ " _|\n",
+
+ " _ \n"+
+ "|_ \n"+
+ "|_|\n",
+
+ " _ \n"+
+ "  |\n"+
+ "  |\n",
+
+ " _ \n"+
+ "|_|\n"+
+ "|_|\n",
+
+ " _ \n"+
+ "|_|\n"+
+ " _|\n",
+]
+
+  def initialize(text)
+    @lines = text.split("\n\n").map do |line|
+      len = ((line.split("\n").map(&:length).max - 1)/ 3) * 3 + 3
+      top, *bottom = line.split("\n").map{ |row| ("%-#{len}s" % row).scan(/.{1,3}/) }
+      top.zip(*bottom).map { |char| ("%-3s\n" * 3) % [*char] }
+    end
+  end
+  
+  def convert
+    @lines.map do |line| 
+      line.map{ |char| @@digits.index(char) || "?" }.map(&:to_s).join
+    end.join(',')
+  end
+end
+
