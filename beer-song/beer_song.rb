e3cfe496f2b288e6fb3522ab1e4ef013f6de3a6d
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-02-12 11:05:02 -0800

    Updated beersong

--- beer-song/beer_song.rb
+++ beer-song/beer_song.rb
@@ -1,29 +1,31 @@
 class BeerSong
   # Sings the beer song
+  VERSION=2
 
   def verse(v)
     # Sings a verse of the beer song
-    if v == 2
-      return "2 bottles of beer on the wall, 2 bottles of beer.\n" \
+    case v
+    when 2
+        "2 bottles of beer on the wall, 2 bottles of beer.\n" \
         "Take one down and pass it around, 1 bottle of beer on the wall.\n"
-    elsif v == 1
-      return "1 bottle of beer on the wall, 1 bottle of beer.\n" \
+    when 1
+        "1 bottle of beer on the wall, 1 bottle of beer.\n" \
         "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    elsif v == 0
-      return "No more bottles of beer on the wall, no more bottles of beer.\n" \
+    when 0
+        "No more bottles of beer on the wall, no more bottles of beer.\n" \
         "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
-      return "#{v} bottles of beer on the wall, #{v} bottles of beer.\n" \
+        "#{v} bottles of beer on the wall, #{v} bottles of beer.\n" \
         "Take one down and pass it around, #{v-1} bottles of beer on the wall.\n"
     end
   end
 
   def verses(start, stop)
     # Sings a set of verses of the beer song
-    start.downto(stop).map {|v| verse(v) + "\n" }.join
+    start.downto(stop).map {|v| verse(v) }.join("\n")
   end
 
-  def sing
+  def lyrics
     # Sings all of the verses of the beer song
     verses(99, 0)
   end
