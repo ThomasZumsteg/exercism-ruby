152c98b8a0f5b944040de91f5997e428dcd3bb8c
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-02-13 20:14:08 -0800

    Updated binary for Ruby

--- binary/binary.rb
+++ binary/binary.rb
@@ -1,12 +1,13 @@
 class Binary
   # Converts binary strings to decimal equivalents
+  VERSION=1
   def initialize(bin)
+    raise ArgumentError.new("#{bin} is not a valid binary number") if bin =~ /[^10]/
     @bin = bin
   end
 
   # attr_reader considered, but makes validation difficult
   def to_decimal
-    return 0 if @bin =~ /[^10]/
     return @bin.each_char.inject(0) { |sum, digit| sum * 2 + digit.to_i }
   end
 end
--- binary/binary_test.rb
+++ binary/binary_test.rb
@@ -1,7 +1,13 @@
+#!/usr/bin/env ruby
+gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'binary'
 
 class BinaryTest < Minitest::Test
+  def test_binary_0_is_decimal_0
+    assert_equal 0, Binary.new('0').to_decimal
+  end
+
   def test_binary_1_is_decimal_1
     assert_equal 1, Binary.new('1').to_decimal
   end
@@ -30,7 +36,27 @@ class BinaryTest < Minitest::Test
     assert_equal 1128, Binary.new('10001101000').to_decimal
   end
 
-  def test_invalid_binary_is_decimal_0
-    assert_equal 0, Binary.new('carrot123').to_decimal
+  def test_binary_ignores_leading_zeros
+    assert_equal 31, Binary.new('000011111').to_decimal
+  end
+
+  def test_invalid_binary_numbers_raise_an_error
+    %w(012 10nope nope10).each do |input|
+      assert_raises ArgumentError do
+        Binary.new(input)
+      end
+    end
+  end
+
+  # Problems in exercism evolve over time,
+  # as we find better ways to ask questions.
+  # The version number refers to the version of the problem you solved,
+  # not your solution.
+  #
+  # Define a constant named VERSION inside of Binary.
+  # If you're curious, read more about constants on RubyDoc:
+  # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
+  def test_bookkeeping
+    assert_equal 1, Binary::VERSION
   end
 end
