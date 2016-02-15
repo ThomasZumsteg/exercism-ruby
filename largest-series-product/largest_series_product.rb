17b5fe650b6c2e36152e157dd6a62b69eb66d7ed
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-02-15 09:19:37 -0800

    Updated largest series product to match unit tests Added version Added guard in initialize Changed largest_product to return 1 only if slice size is 0

--- largest-series-product/largest_series_product.rb
+++ largest-series-product/largest_series_product.rb
@@ -1,7 +1,9 @@
 class Series
+  VERSION=1
   attr_reader :digits
 
   def initialize(digits)
+    raise ArgumentError if digits =~ /[^0-9]/
     @digits = digits.each_char.map(&:to_i)
   end
 
@@ -11,7 +13,7 @@ class Series
   end
 
   def largest_product(size)
-    return 1 if size <= 0
+    return 1 if size == 0
     slices(size).map { |set| set.inject(:*) }.max
   end
 end
