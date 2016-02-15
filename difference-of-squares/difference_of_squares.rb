74a5f44ed6b25dd67715348d77fe0dbe5af9f8a1
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-02-14 22:04:10 -0800

    Updated difference-of-squares to match V2 of the unit tests

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,15 +1,16 @@
 class Squares
   # Square sequence sums and differences
+  VERSION=2
 
   def initialize(num)
     @num = num
   end
 
   def difference
-    square_of_sums - sum_of_squares
+    square_of_sum - sum_of_squares
   end
 
-  def square_of_sums 
+  def square_of_sum
     @square_of_sums ||= @num**2 * (@num + 1)**2 / 4
   end
 
